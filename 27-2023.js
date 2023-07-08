c7baf1d698e7a86e2458e2919cfae4eca6a7ffba
Alessandra Maffei <113006001+a-maffei@users.noreply.github.com>
2023-07-08 18:48:15 +0200

    Update naming 27

--- 27-2023.js
+++ 27-2023.js
@@ -1,13 +1,13 @@
 /* Given an integer n, return true if it's a perfect square AND when reversed, is still a perfect square. */
 
 function reversedSquares(number) {
-  function isSquareRoot(n) {
+  function isPerfectSquare(n) {
     Number.isInteger(Math.sqrt(n));
   }
 
   const reversed = parseInt(number.toString().split("").reverse().join(""));
 
-  return isSquareRoot(number) && isSquareRoot(reversed);
+  return isPerfectSquare(number) && isPerfectSquare(reversed);
 }
 
 reversedSquares(9);
