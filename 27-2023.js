74353db32013160107db0f025debd43c877eca5a
Alessandra Maffei <ales.maffei@gmail.com>
2023-07-08 18:46:25 +0200

    week 27

---
+++ 27-2023.js
@@ -0,0 +1,15 @@
+/* Given an integer n, return true if it's a perfect square AND when reversed, is still a perfect square. */
+
+function reversedSquares(number) {
+  function isSquareRoot(n) {
+    Number.isInteger(Math.sqrt(n));
+  }
+
+  const reversed = parseInt(number.toString().split("").reverse().join(""));
+
+  return isSquareRoot(number) && isSquareRoot(reversed);
+}
+
+reversedSquares(9);
+reversedSquares(441);
+reversedSquares(25);
